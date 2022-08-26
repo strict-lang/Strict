7d1551c8a1541dfc038f0abd83a078d192fb7ced
Lukaa <lukaminjoraia@gmail.com>
2022-08-26 17:04:33 +0200

    Remove useless tests from previous commit

--- Strict.Language.Expressions.Tests/MethodCallTests.cs
+++ Strict.Language.Expressions.Tests/MethodCallTests.cs
@@ -93,8 +93,6 @@ public sealed class MethodCallTests : TestExpressions
 	[TestCase("Count(5).Increment")]
 	[TestCase("Count(5).Floor")]
 	[TestCase("Range(0, 10)")]
-	[TestCase("Range(5, 10)")]
-	[TestCase("Range(5, 10).Length")]
 	[TestCase("Range(0, 10).Length")]
 	public void FromExample(string fromMethodCall) =>
 		Assert.That(ParseExpression(fromMethodCall).ToString(), Is.EqualTo(fromMethodCall));
