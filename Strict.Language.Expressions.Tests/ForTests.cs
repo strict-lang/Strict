07c297d8d4aee5fe8be69a43ef2b09dd2d14782e
Lukaa <lukaminjoraia@gmail.com>
2022-09-06 18:56:25 +0200

    For - some tests and initial parsing, needs way more tests

---
+++ Strict.Language.Expressions.Tests/ForTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Strict.Language.Expressions.Tests;
+
+public class ForTests : TestExpressions
+{
+	[Test]
+	public void MissingBody() =>
+		Assert.That(() => ParseExpression("for Range(2,5)"),
+			Throws.InstanceOf<For.MissingInnerBody>());
+
+	[Test]
+	public void MissingExpression() =>
+		Assert.That(() => ParseExpression("for"), Throws.InstanceOf<For.MissingExpression>());
+
+	[Test]
+	public void ParseForExpression()
+	{
+		var forExpression = (For)ParseExpression("for Range(2, 5)", "\tlog.Write(\"Hi\")");
+		Assert.That(forExpression.Body, Is.EqualTo(ParseExpression("log.Write(\"Hi\")")));
+		Assert.That(forExpression.Value, Is.EqualTo(ParseExpression("Range(2, 5)")));
+		Assert.That(forExpression.ToString(), Is.EqualTo("for Range(2, 5)"));
+	}
+
+	[Test]
+	public void InvalidForExpression() =>
+		Assert.That(() => ParseExpression("for gibberish", "\tlet num = 5"),
+			Throws.InstanceOf<IdentifierNotFound>());
+}
--- Strict.Language.Expressions/For.cs
+++ Strict.Language.Expressions/For.cs
@@ -5,25 +5,40 @@ namespace Strict.Language.Expressions;
 //TODO: docusaurus link for all expressions!
 public sealed class For : Expression
 {
-	public For(Expression value) : base(value.ReturnType) => Value = value;
-	public Expression Value { get; } //TODO: for Range, for list, etc.
+	private const string ForName = "for";
 
-	//TODO: public Expression Body { get; }
+	public For(Expression value, Expression body) : base(value.ReturnType)
+	{
+		Value = value;
+		Body = body;
+	}
+
+	public Expression Value { get; } //TODO: for Range, for list, etc.
+	public Expression Body { get; }
 	public override int GetHashCode() => Value.GetHashCode();
 	public override string ToString() => $"for {Value}";
 	public override bool Equals(Expression? other) => other is For a && Equals(Value, a.Value);
 
-	public static Expression? TryParse(Body body, ReadOnlySpan<char> line) =>
-		line.StartsWith(ForName, StringComparison.Ordinal)
-			? new For(line.Length <= ForName.Length
-				? throw new MissingExpression(body)
-				: body.Method.ParseExpression(body, line[4..]))
-			: null;
-
-	private const string ForName = "for";
+	public static Expression? TryParse(Body body, ReadOnlySpan<char> line)
+	{
+		if (!line.StartsWith(ForName, StringComparison.Ordinal))
+			return null;
+		if (line.Length <= ForName.Length)
+			throw new MissingExpression(body);
+		var innerBody = body.FindCurrentChild();
+		if (innerBody == null)
+			throw new MissingInnerBody(body);
+		var bodyExpression = innerBody.Parse();
+		return new For(body.Method.ParseExpression(body, line[4..]), bodyExpression);
+	}
 
 	public sealed class MissingExpression : ParsingFailed
 	{
 		public MissingExpression(Body body) : base(body) { }
+	}
+
+	public sealed class MissingInnerBody : ParsingFailed
+	{
+		public MissingInnerBody(Body body) : base(body) { }
 	}
 }
