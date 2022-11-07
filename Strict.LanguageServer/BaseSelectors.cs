ac37f5aac12a4b693381258033493a68724ee208
Lukaa <lukaminjoraia@gmail.com>
2022-11-07 17:47:27 +0100

    Add base selectors

---
+++ Strict.LanguageServer/BaseSelectors.cs
@@ -0,0 +1,10 @@
+﻿using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Strict.LanguageServer
+{
+	public static class BaseSelectors
+	{
+		public static readonly DocumentSelector StrictDocumentSelector = new(
+			new DocumentFilter { Pattern = "**/*.strict" });
+	}
+}
