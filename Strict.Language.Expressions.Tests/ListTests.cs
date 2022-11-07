2ea4b7c8e1bb8b94c99118a241bf07ef10f4d30d
Murali <muralitandabany@gmail.com>
2022-11-07 16:59:55 +0100

    Case 26394: Added MultiLineExpression test with texts input

--- Strict.Language.Expressions.Tests/ListTests.cs
+++ Strict.Language.Expressions.Tests/ListTests.cs
@@ -293,34 +293,58 @@ public sealed class ListTests : TestExpressions
 			Throws.InstanceOf<Type.UnterminatedMultiLineListFound>().With.Message.
 				StartWith("\tlet result = (1, 2, 3, 4,"));
 
-	[Test]
-	public void ParseMultiLineExpressionAndPrintSameAsInput()
+	// @formatter:off
+	[TestCase("ParseMultiLineExpressionWithNumbers", "(numbers(0),\n\tnumbers(1),\n\tnumbers(2),\n\tnumbers(3),\n\tnumbers(4),\n\tnumbers(5)," +
+		"\n\tnumbers(6),\n\tanotherNumbers(0),\n\tanotherNumbers(1),\n\tanotherNumbers(2))", "has numbers",
+		"has anotherNumbers Numbers",
+		"Run Numbers",
+		"\t(numbers(0),",
+		"\tnumbers(1),",
+		"\tnumbers(2),",
+		"\tnumbers(3),",
+		"\tnumbers(4),",
+		"\tnumbers(5),",
+		"\tnumbers(6),",
+		"\tanotherNumbers(0),",
+		"\tanotherNumbers(1),",
+		"\tanotherNumbers(2))",
+		"ExtraMethodNotCalled",
+		"\tsomething")]
+	[TestCase("ParseMultiLineExpressionWithText", "(\"somethingsomethingsomething + 5\","+
+						"\n\t\"somethingsomethingsomethingsomething\","+
+						"\n\t\"somethingsomethingsomethingsomething\","+
+						"\n\t\"somethingsomethingsomethingsomething\","+
+						"\n\t\"somethingsomethingsomethingsomething\","+
+						"\n\t\"somethingsomethingsomethingsomething\","+
+						"\n\t\"somethingsomethingsomethingsomething\","+
+						"\n\t\"somethingsomethingsomethingsomething\","+
+						"\n\t\"somethingsomethingsomethingsomething\","+
+						"\n\t\"somethingsomethingsomethingsomething\","+
+						"\n\t\"somethingsomethingsomethingsomething\")",
+						"has log",
+					"Run Numbers",
+					"\t(\"somethingsomethingsomething + 5\",",
+					"\t\"somethingsomethingsomethingsomething\",",
+					"\t\"somethingsomethingsomethingsomething\",",
+					"\t\"somethingsomethingsomethingsomething\",",
+					"\t\"somethingsomethingsomethingsomething\",",
+					"\t\"somethingsomethingsomethingsomething\",",
+					"\t\"somethingsomethingsomethingsomething\",",
+					"\t\"somethingsomethingsomethingsomething\",",
+					"\t\"somethingsomethingsomethingsomething\",",
+					"\t\"somethingsomethingsomethingsomething\",",
+					"\t\"somethingsomethingsomethingsomething\")",
+					"ExtraMethodNotCalled",
+					"\tsomething")]
+	// @formatter:on
+	public void ParseMultiLineExpressionAndPrintSameAsInput(string testName, string expected, params string[] code)
 	{
 		var program = new Type(type.Package,
-				new TypeLines(nameof(ParseMultiLineExpressionAndPrintSameAsInput),
-					// @formatter:off
-					"has numbers",
-					"has anotherNumbers Numbers",
-					"Run Numbers",
-					"\t(numbers(0),",
-					"\tnumbers(1),",
-					"\tnumbers(2),",
-					"\tnumbers(3),",
-					"\tnumbers(4),",
-					"\tnumbers(5),",
-					"\tnumbers(6),",
-					"\tanotherNumbers(0),",
-					"\tanotherNumbers(1),",
-					"\tanotherNumbers(2))",
-					"ExtraMethodNotCalled",
-					"\tsomething"))
-			// @formatter:off
-			.ParseMembersAndMethods(parser);
+				new TypeLines(testName, code)).
+			ParseMembersAndMethods(parser);
 		var expression = program.Methods[0].GetBodyAndParseIfNeeded();
 		Assert.That(expression, Is.InstanceOf<List>());
-		Assert.That(expression.ToString(),
-			Is.EqualTo("(numbers(0),\n\tnumbers(1),\n\tnumbers(2),\n\tnumbers(3),\n\tnumbers(4),\n\tnumbers(5),"+
-				"\n\tnumbers(6),\n\tanotherNumbers(0),\n\tanotherNumbers(1),\n\tanotherNumbers(2))"));
+		Assert.That(expression.ToString(), Is.EqualTo(expected));
 		Assert.That(program.Methods[1].lines.Count, Is.EqualTo(2));
 	}
 }
