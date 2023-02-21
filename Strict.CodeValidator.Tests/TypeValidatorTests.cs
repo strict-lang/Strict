51fc83422cf12cdfbfa8ee726e2e4b9a6df5b759
MuraliTandabany <40332490+MuraliTandabany@users.noreply.github.com>
2023-02-21 18:05:20 +0100

    Case 27349: Added TypeValidator to check UnusedMember use case but code is still in progress and not completed yet

---
+++ Strict.CodeValidator.Tests/TypeValidatorTests.cs
@@ -0,0 +1,15 @@
+﻿using Strict.Language;
+using Strict.Language.Tests;
+using Type = Strict.Language.Type;
+
+namespace Strict.CodeValidator.Tests;
+
+public sealed class TypeValidatorTests
+{
+	[Test]
+	public void ValidateUnusedMember()
+	{
+		var type = new Type(new TestPackage(), new TypeLines(nameof(ValidateUnusedMember), ""));
+		var typeValidator = new TypeValidator(new[] { type });
+	}
+}
---
+++ Strict.CodeValidator/TypeValidator.cs
@@ -0,0 +1,11 @@
+﻿using Type = Strict.Language.Type;
+
+namespace Strict.CodeValidator;
+
+public sealed record TypeValidator(IEnumerable<Type> Types) : Validator
+{
+	public void Validate()
+	{
+
+	}
+}
