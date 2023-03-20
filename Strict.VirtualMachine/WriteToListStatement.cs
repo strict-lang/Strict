70eea72da5bbc02194a1c6d13b016577721cdb58
Lukaa <lukaminjoraia@gmail.com>
2023-03-20 16:36:02 +0100

    fix wrong commit

--- Strict.VirtualMachine/WriteToListStatement.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Strict.VirtualMachine;
-
-public sealed class WriteToListStatement : RegisterStatement
-{
-	public string Identifier { get; }
-	public WriteToListStatement(Register register, string identifier) : base(register, Instruction.WriteToList) => Identifier = identifier;
-}
